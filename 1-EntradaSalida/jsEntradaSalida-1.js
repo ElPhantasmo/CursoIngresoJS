79e14988da223ebbb98c5079769c70ae67b17afc
Lucascabane <56005890+Lucascabane@users.noreply.github.com>
2020-02-06 11:12:47 -0300

    ejercicio uno
    edit ejercicio uno

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,19 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	
+	var primerPrecio; //se escriben nombres completos de variables
+	var segundoPrecio;
+	var apellido="nadie";
+	
+
+	apellido="Perez";
+	primerPrecio="$420"
+
+	//alert("Esto funciona de maravilla"); //Esto es un valor literal
+	//alert("tal cual");
+	alert (apellido);
+	//alert(); //sale vacio
+	alert (primerPrecio);
 }
 
