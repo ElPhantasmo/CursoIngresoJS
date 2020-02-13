44a1aae5c9db59ecb01312433a84a30719dfec94
alumno <alumno>
2020-02-13 11:50:54 -0300

    If 1

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
+
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
 //tomo la edad  
+
+	if (edad<13 || edad>17) 
+	{
+		alert("No es adolescente");
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,25 @@
 function mostrar()
 {
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
 //tomo la edad  
+	
+	if (edad>12 && edad<18) 
+	{
+		alert("Es adolescente");
+	}
+
+	else if (edad<13) 
+	{
+		alert("Es un Niño");
+	}
+	
+	else
+	{
+		alert ("Es Adulto");
+	}
 
 
 
