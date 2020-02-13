877ba2437732d2333ac7cb702ae5ebee3e9e638b
alumno <alumno>
2020-02-13 09:44:54 -0300

    curso de GIT

--- CursoDeGIT.js
+++ CursoDeGIT.js
@@ -1,5 +1,7 @@
 git clone https://github.com/Lucascabane/CursoIngresoJS.git
 
-git add.
+git add. //añade todos los cambios (.) a la carpeta local 
 
 git commit -m "mensaje"
+
+git push //sube los commit a github
