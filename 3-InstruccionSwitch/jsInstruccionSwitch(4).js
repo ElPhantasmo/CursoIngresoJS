0bcc8b2b5ec067f9921f5b0233156116f65ddd23
alumno <alumno>
2020-02-20 10:36:21 -0300

    Todo Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,6 +4,22 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+
+	switch(mesDelAño) 
+		{
+			case "Febrero":
+				alert("Este mes tiene 28 dias.");
+				break;
+			case "Abril":
+			case "Junio":
+			case "Septiembre":
+			case "Noviembre":
+				alert("Este mes tiene 30 dias");
+				break;
+			default:
+				alert("Este mes tiene 31 dias");
+				break;
+		}
 	
 	
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -2,7 +2,7 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
-
+laHora = parseInt(laHora);
 
 
 }//FIN DE LA FUNCIÓN
