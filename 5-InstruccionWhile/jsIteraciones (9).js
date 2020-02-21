06d0c69540b28238578fb2bd0258a11963d2f062
alumno <alumno>
2020-02-21 12:20:38 -0300

    While 9 incompleto

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,14 +2,41 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
+	var numeroIngresado;
+	var minimo;
+	var maximo;	
 	var respuesta='si';
+	var primeraVez;
 
 	while(respuesta!='no')
 	{
+		contador = contador + 1;
+		numeroIngresado = prompt("ingrese numero");
+		numeroIngresado = parseInt(numeroIngresado);
 		
+		while (isNaN(numeroIngresado))
+		{
+			numeroIngresado = prompt("Error, ingrese un numero");
+			numeroIngresado = parseInt(numeroIngresado);			
+		}			
 	
+		if (primeraVez = false) 
+			{
+				minimo = numeroIngresado;
+				maximo = numeroIngresado;
+			}
+		else 
+			{
+				if (numeroIngresado > maximo) 
+					{
+						maximo = numeroIngresado;
+					}
+
+				else 
+					{
+						minimo = numeroIngresado;
+					}
+			}
 	}
 
 
